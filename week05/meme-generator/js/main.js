4de40001637ef991e5613cbbf598d65bd5d89bb9
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-10-13 12:06:39 -0600

    ..

--- week05/meme-generator/js/main.js
+++ week05/meme-generator/js/main.js
@@ -1,6 +1,20 @@
 // Enter your JavaScript for the solution here...
 
-// target the image
-let image = document.querySelector("select.meme-image");
+//targetting the form element
+const memeForm = document.querySelector(".meme-form");
 
-console.log("Selected an image");
+//adding an event listener to the form 
+memeForm.addEventListener("submit", function(event) {
+    //console.log("FOrm submitted");
+    
+    event.preventDefault();
+
+
+
+    //creating the canvas element to put our img background
+    let topText = document.getElementById("meme-top-text");
+
+
+
+
+});
