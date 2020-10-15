1cf2198aa744b7646f0cb7a3b8636531e6d1f8d8
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-10-14 23:44:46 -0600

    meme

--- week05/meme-generator/js/main.js
+++ week05/meme-generator/js/main.js
@@ -1,20 +1,30 @@
 // Enter your JavaScript for the solution here...
 
-//targetting the form element
-const memeForm = document.querySelector(".meme-form");
+// //targetting the form element
+// const memeForm = document.querySelector(".meme-form");
 
-//adding an event listener to the form 
-memeForm.addEventListener("submit", function(event) {
-    //console.log("FOrm submitted");
-    
-    event.preventDefault();
+// //adding an event listener to the form 
+// memeForm.addEventListener("submit", function(event) {
+//     //console.log("FOrm submitted");  
+//     event.preventDefault();
 
+//     //creating the canvas element to put our img background
+//     let topText = document.getElementById("meme-top-text");
+// });
 
+//=========================
+//targetting the image
+const memeImage = document.querySelector("#meme-image")
+memeImage.addEventListener("change", function(e){
+    const selectedImage = e.currentTarget.options[e.currentTarget.selectedIndex];
+    //call and display the target image
+    updateImage(e.currentTarget.value)
+})
 
-    //creating the canvas element to put our img background
-    let topText = document.getElementById("meme-top-text");
-
-
-
-
-});
+// Function that updates the image
+// which image
+function updateImage(whichImage){
+    const memeDisplay = document.querySelector(".meme-display img")
+    const path = '/img/${whichImage}.png';    //
+    memeDisplay.src = path;
+}
