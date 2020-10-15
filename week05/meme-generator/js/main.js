b968ac159ba8b2a43456a7b30d9ab719c9d4a15a
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-10-15 11:50:32 -0600

    working in progress

--- week05/meme-generator/js/main.js
+++ week05/meme-generator/js/main.js
@@ -12,19 +12,42 @@
 //     let topText = document.getElementById("meme-top-text");
 // });
 
-//=========================
-//targetting the image
-const memeImage = document.querySelector("#meme-image")
-memeImage.addEventListener("change", function(e){
-    const selectedImage = e.currentTarget.options[e.currentTarget.selectedIndex];
-    //call and display the target image
-    updateImage(e.currentTarget.value)
-})
+//=========FORM ELEMENTS================
+//=========1. SELECT THE TARGET ELEMENT
+// target the form
+const memeForm = document.querySelector(".meme-form");
+// target where meme will be displayed
+const memeDisplay = document.querySelector(".meme-display img");
 
-// Function that updates the image
-// which image
-function updateImage(whichImage){
-    const memeDisplay = document.querySelector(".meme-display img")
-    const path = '/img/${whichImage}.png';    //
-    memeDisplay.src = path;
-}
+//========2. CREATE THE EVENT LISTENER FUNCTION
+//function to display image when user submit or generate the button
+memeForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+
+    // Function that updates the image
+    function updateImage(whichImage){
+        const memeDisplay = document.querySelector(".meme-display img")
+        const path = `img/${whichImage}.png` // to access the img folder
+        memeDisplay.src = path;
+    }
+
+    //targetting list of images from other folder
+    const memeImage = document.querySelector("#meme-image")
+    memeImage.addEventListener("change", function(e){
+        const selectedImage = e.currentTarget.options[e.currentTarget.selectedIndex];
+        //call and display the target image
+        updateImage(e.currentTarget.value)
+    })
+    //updateImage(memeImage);
+    
+});
+// //===============GENERATE BTN============
+// const generateBtn = document.querySelector("input[type=submit]");
+
+// // // this is for testing on console
+// // function generateBtnClick() {
+// //     console.log("You click generate button");
+// // }
+
+// generateBtn.addEventListener("click", updateImage);
