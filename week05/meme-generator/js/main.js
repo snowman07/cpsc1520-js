c81fc24b05b4f0e7f162e0c74dc53b6458245450
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-10-16 06:49:27 -0600

    DONE on display image and text. Validation almost done. TODO: reset event listener and some validations

--- week05/meme-generator/js/main.js
+++ week05/meme-generator/js/main.js
@@ -1,52 +1,55 @@
-// Enter your JavaScript for the solution here...
+document.addEventListener("DOMContentLoaded", function(){
+    
+    //=========1. SELECT THE TARGET ELEMENT
+    // target the form
+    const memeForm = document.querySelector(".meme-form");
+    // target where meme will be displayed
+    const memeDisplay = document.querySelector(".meme-display img");
 
-// //targetting the form element
-// const memeForm = document.querySelector(".meme-form");
+    //========2. CREATE THE EVENT LISTENER FUNCTION
+    //function to display image when user submit or generate the button
 
-// //adding an event listener to the form 
-// memeForm.addEventListener("submit", function(event) {
-//     //console.log("FOrm submitted");  
-//     event.preventDefault();
 
-//     //creating the canvas element to put our img background
-//     let topText = document.getElementById("meme-top-text");
-// });
 
-//=========FORM ELEMENTS================
-//=========1. SELECT THE TARGET ELEMENT
-// target the form
-const memeForm = document.querySelector(".meme-form");
-// target where meme will be displayed
-const memeDisplay = document.querySelector(".meme-display img");
-
-//========2. CREATE THE EVENT LISTENER FUNCTION
-//function to display image when user submit or generate the button
-memeForm.addEventListener("submit", function(event){
-    event.preventDefault();
-
-    //targetting list of images from other folder
     const memeImage = document.querySelector("#meme-image")
     memeImage.addEventListener("change", function(e){
         const selectedImage = e.currentTarget.options[e.currentTarget.selectedIndex];
         //call and display the target image
         updateImage(e.currentTarget.value)
     })
-    //updateImage(memeImage);
 
-    // Function that updates the image
+    // Function targetting list of images from other folder
     function updateImage(whichImage){
         const memeDisplay = document.querySelector(".meme-display img")
+        //memeDisplay;
         const path = `img/${whichImage}.png` // to access the img folder
         memeDisplay.src = path;
+        memeDisplay.alt = path;
     }
     
+    
+    memeForm.addEventListener("submit", function(event){
+        
+        //console.log("Forms submitted");
+
+        let memeForm = event.target; // need to target the form inside submit event
+        let topText = memeForm.elements.memeTopText; // to get the value of TOPTEXT
+        let bottomText = memeForm.elements.memeBottomText; // to get the value of BOTTOMTEXT     
+        let error = document.querySelector(".error"); // to display error message
+
+        // VALIDATION
+        if ((topText.value.trim() == "") || (bottomText.value.trim() == "")){
+            error.innerHTML = "Top and bottom text cannot be blank";
+        } else {
+            document.querySelector("p.top-text").innerHTML = topText.value; //to display the input
+            topText.value = ""; // remove the text after submit/generate
+            document.querySelector("p.bottom-text").innerHTML = bottomText.value; //to display the input
+            bottomText.value = ""; // remove the text after submit/generate
+        } 
+
+        event.preventDefault();
+        
+    });
+
 });
-// //===============GENERATE BTN============
-// const generateBtn = document.querySelector("input[type=submit]");
 
-// // // this is for testing on console
-// // function generateBtnClick() {
-// //     console.log("You click generate button");
-// // }
-
-// generateBtn.addEventListener("click", updateImage);
