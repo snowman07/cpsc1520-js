1fcff81d3f1a9d256391b6f83b3daeb0545f982d
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-10-21 15:34:08 -0600

    exercise A done

--- week07/exercise/js/main.js
+++ week07/exercise/js/main.js
@@ -1,1 +1,18 @@
 // Enter JavaScript for the exercise here...
+
+document.querySelector("[name=toggle]").addEventListener("change", function(evt) {
+    let checks = document.querySelectorAll("[name=message]");
+    //console.log(checks);
+
+    let idx = 0;
+     
+    while (idx < checks.length) {
+        if (evt.target.checked) {
+            checks[idx].checked = true;
+        } else {
+            checks[idx].checked = false;
+        }
+
+        idx = idx + 1;
+    }
+});
