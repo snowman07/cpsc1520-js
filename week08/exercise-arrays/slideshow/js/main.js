dfed8078e56fe80d08f3c3b308c0fddb4b2ca577
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-10-22 17:45:08 -0600

    need to figure out how carousel works

--- week08/exercise-arrays/slideshow/js/main.js
+++ week08/exercise-arrays/slideshow/js/main.js
@@ -1,1 +1,27 @@
 // your js here...
+let images = ['mountain1.jpg', 'mountain2.jpg', 'mountain3.jpg'];
+let currentImg = 0;
+
+document.querySelector(".carousel>img").src = "images/" + images[currentImg];
+
+document.querySelector(".carousel").addEventListener("click", function (evt) {
+    let target = evt.target;
+    if (target.classList.contains("control")) {
+        //console.log("control clicked...");
+        if (target.classList.contains("next")){
+            // move to the next index in the array
+            currentImg += 1;
+            //continue;
+        } else if (target.classList.contains("prev")) {
+            //move to the previous index in the array
+            currentImg -= 1;
+            //continue;
+            
+        }
+        //continue;
+        // display the new current image
+        document.querySelector(".carousel>img").src = "images/" + images[currentImg];
+        //break;
+        //continue;
+    }
+});
