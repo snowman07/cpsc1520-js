fb5abe55ea6328ad33acf26e256144782307af58
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-11-04 08:52:26 -0700

    TODO: figure out the filter

--- week07/ass2-photo-filter/js/main.js
+++ week07/ass2-photo-filter/js/main.js
@@ -44,30 +44,87 @@
 // })
 // //===================================================
 
+///////////// START HERE
+//////PART A - user input search here
+const searchBar = document.querySelector("#filter"); //filter functionality
+//const tags = document.querySelectorAll(".tags");      //search all tags
+
+///// PART B - declare search and reset function here
+
+// Add an input event listener to the input
+searchBar.addEventListener("input", onSearchItems);
+
+function onSearchItems(e){
+    const searchTerm = e.currentTarget.value.trim().toLowerCase();
+
+    if(searchTerm === ""){  // get PART D - function for reset
+        resetTheDisplay();
+    } else {    // get PART C - function for search
+        searchForMatchingItems(searchTerm);
+    }
+}
 
 
-// search here
+///// PART C - function for search
 
-//////// Fill out the stats part using map filter and reduce.
+function searchForMatchingItems(searchTerm){
+    // get all the thumb-display and the tags of it
+    let thumbDisplay = document.querySelectorAll(".thumb-display");
+    //let tags = document.querySelectorAll(".tags");
+    const tags = document.querySelectorAll(".tags");      //search all tags
+    //// convert it into Array
+    let thumbDisplayArray = Array.from(thumbDisplay);
+    let tagsArray = Array.from(tags);
 
-// get all the thumb-display and the tags of it
-let thumbDisplay = document.querySelectorAll(".thumb-display");
-let tags = document.querySelectorAll(".tags");
-// convert it into Array
-let thumbDisplayArray = Array.from(thumbDisplay);
-let tagsArray = Array.from(tags);
-
-//filter the thumb-display for a given tag
-let tag = "#mountain"; //CHANGE THIS TO WHAT USER INPUT IN FILTER // this is where trev go to on filter thingy
-
-//filter thumbDisplayArray using children
-let thumbDisplayWithTags = thumbDisplayArray.filter((thumbdisplay) => {
+    //filter thumbDisplayArray using children
+    let thumbDisplayWithTags = thumbDisplayArray.filter((thumbdisplay) => {
     // console.log(thumb-display);
     // console.log(thumb-display.children[1].innerText);
     // console.log("------");
-    if(thumbdisplay.children[1].innerText.includes(tag)){ // condition must be from user input in filter
-        return true; // this is where trev go to
-    }
-    return false;
-});
-console.log(thumbDisplayWithTags);
+        if(thumbdisplay.children[1].innerText.includes(tag)){ // condition must be from user input in filter
+            return true; // this is where trev go to
+        }
+        return false;
+    });
+    console.log(thumbDisplayWithTags);
+
+}
+// // get all the thumb-display and the tags of it
+// let thumbDisplay = document.querySelectorAll(".thumb-display");
+// //let tags = document.querySelectorAll(".tags");
+// const tags = document.querySelectorAll(".tags");      //search all tags
+// //// convert it into Array
+// let thumbDisplayArray = Array.from(thumbDisplay);
+// let tagsArray = Array.from(tags);
+
+// //filter the thumb-display for a given tag
+// let tag = "#river"; //CHANGE THIS TO WHAT USER INPUT IN FILTER // this is where trev go to on filter thingy
+
+// //filter thumbDisplayArray using children
+// let thumbDisplayWithTags = thumbDisplayArray.filter((thumbdisplay) => {
+//     // console.log(thumb-display);
+//     // console.log(thumb-display.children[1].innerText);
+//     // console.log("------");
+//     if(thumbdisplay.children[1].innerText.includes(tag)){ // condition must be from user input in filter
+//         return true; // this is where trev go to
+//     }
+//     return false;
+// });
+// console.log(thumbDisplayWithTags);
+
+
+
+
+//// PART D - function for reset
+
+
+//======
+
+
+//////// Fill out the stats part using map filter and reduce.
+
+
+
+
+
+
