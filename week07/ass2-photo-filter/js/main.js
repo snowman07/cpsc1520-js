72714583dc3d4763a49bd5e36ae0cd489915cd79
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-11-06 08:30:01 -0700

    DONE: display image. TODO: reset shit

--- week07/ass2-photo-filter/js/main.js
+++ week07/ass2-photo-filter/js/main.js
@@ -1,80 +1,5 @@
-// Enter your JavaScript for the solution here...
-
-// //===================TREVCODE================================
-// window.addEventListener('load', function(e){
-//     const FILTERDISPLAY = document.querySelector("#filter.frm-control"); //filter functionality
-//     const TAGS = document.querySelectorAll(".tags"); //all tags
-
-//     // Add an input event listener to the input
-//     FILTERDISPLAY.addEventListener("input", onSearchItems);
-
-//     function onSearchItems(e){
-//         const SEARCHTERM = e.currentTarget.value.trim().toLowerCase()
-
-//         if(SEARCHTERM === "") {
-//             resetTheDisplay();
-//         } else {
-//             // search through the array looking for matches
-//             searchForMatchingItems(SEARCHTERM);
-//         }
-//     }
-
-//     function searchForMatchingItems(SEARCHTERM){
-//         TAGS.forEach(function(tags,index) {
-//             const ITEM = tags.textContent.trim().toLowerCase();
-//                 if(ITEM.includes(SEARCHTERM)){
-//                     tags.classList.add('animate__fadeIn'); //
-//                     if(tags.classList.contains('animate__fadeOut')){ //
-//                         tags.classList.remove('animate__fadeOut') //
-//                     } //
-//                 } else {
-//                     tags.classList.add('animate__fadeOut'); //
-//                 }
-//         })
-//     }
-
-//     function resetTheDisplay() {
-//         TAGS.forEach(function(tags, index) {
-//             if(tags.classList.contains('animate__fadeOut')){
-//                 tags.classList.remove('animate__fadeOut');
-//             }
-//         })
-//     }
-
-// })
-// //===================TREVCODE================================
 
 
-// // //===================CLASCODE================================
-// //////// Fill out the stats part using map filter and reduce.
-
-// // get all the thumb-display and the tags of it
-// let thumbDisplay = document.querySelectorAll(".thumb-display");
-// let tags = document.querySelectorAll(".tags");
-// // convert it into Array
-// let thumbDisplayArray = Array.from(thumbDisplay);
-// let tagsArray = Array.from(tags);
-
-// //filter the thumb-display for a given tag
-// let tag = "#river"; //CHANGE THIS TO WHAT USER INPUT IN FILTER // this is where trev go to on filter thingy
-
-// //filter thumbDisplayArray using children
-// let thumbDisplayWithTags = thumbDisplayArray.filter((thumbdisplay) => {
-//     // console.log(thumb-display);
-//     // console.log(thumb-display.children[1].innerText);
-//     // console.log("------");
-//     if(thumbdisplay.children[1].innerText.includes(tag)){ // condition must be from user input in filter
-//         return true; // this is where trev go to
-//     }
-//     return false;
-// });
-// console.log(thumbDisplayWithTags);
-// // //===================CLASCODE================================
-
-
-
-
-///////////// START HERE
 //////PART A - user input search here
 const searchBar = document.querySelector("#filter"); //filter functionality
 //const tags = document.querySelectorAll(".tags");      //search all tags
@@ -83,17 +8,22 @@ const searchBar = document.querySelector("#filter"); //filter functionality
 
 // Add an input event listener to the input
 searchBar.addEventListener("input", onSearchItems);
-
+//-----
 function onSearchItems(e){
     const searchTerm = e.currentTarget.value.trim().toLowerCase();
 
-    if(searchTerm === ""){  // get PART D - function for reset
-        resetTheDisplay();
-    } else {    // get PART C - function for search
-        searchForMatchingItems(searchTerm);
-    }
-}
+    // if(searchTerm === ""){  // get PART D - function for reset
+    //     resetTheDisplay();
+    // } else {    // get PART C - function for search
+    //     searchForMatchingItems(searchTerm);
+    // }
 
+    searchForMatchingItems(searchTerm);
+}
+//-----
+
+
+//searchBar.addEventListener("input", searchForMatchingItems);
 
 ///// PART C - function for search
 
@@ -101,33 +31,53 @@ function searchForMatchingItems(searchTerm){
     // get all the thumb-display and the tags of it
     let thumbDisplay = document.querySelectorAll(".thumb-display");
     //let tags = document.querySelectorAll(".tags");
-    const tags = document.querySelectorAll(".tags");      //search all tags
+    let tags = document.querySelectorAll(".tags");      //search all tags
+    
     //// convert it into Array
     let thumbDisplayArray = Array.from(thumbDisplay);
     let tagsArray = Array.from(tags);
 
     //filter thumbDisplayArray using children
-    let thumbDisplayWithTags = thumbDisplayArray.filter((thumbdisplay) => {
+    let thumbDisplayWithTags = thumbDisplayArray.filter((thumbdisplay) => { //thumbdisplay is a callback function
     // console.log(thumb-display);
     // console.log(thumb-display.children[1].innerText);
     // console.log("------");
-        if(thumbdisplay.children[1].innerText.includes(tag)){ // condition must be from user input in filter
-            return true; // this is where trev go to
-        }
-        return false;
+    
+        if(!thumbdisplay.children[1].innerText.includes(searchTerm)){ // if NOT in the searchTerm (filter)
+            // image will not stay
+            //return true; 
+            //console.log("capt image");
+            //console.log(thumbdisplay);
+            thumbdisplay.style.display = "none"; // THIS IS THE ANSWER BABY!!!!!
+            //if there's a text in the filter
+            let resetShow = document.querySelector("a.reset");
+            if (resetShow.classList.contains("hidden")){
+                //resetShow.classList.add("hidden");
+                resetShow.style.visibility = "visible"; // THIS IS THE ANSWER BABY!!!!!
+            } else {
+                resetShow.style.display = "none"; // THIS IS THE ANSWER BABY!!!!!
+            }
+            
+        } 
+        //else {
+            // image will fadeout 
+            //return false;   
+            
+        //}
+        
     });
-    console.log(thumbDisplayWithTags);
 
 }
 
-
 //// PART D - function for reset
+function resetTheDisplay(searchTerm) {
+    let resetShow = document.querySelector("a.reset");
+    if(searchBar.contains(searchTerm)){
+        resetShow.classList.add("hidden");
+    }
+}
 
 
-//======
-
-
-//////// Fill out the stats part using map filter and reduce.
 
 
 
