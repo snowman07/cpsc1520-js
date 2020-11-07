6be46835e3f0669a18fe0d598cbcbd2af8299885
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-11-06 23:07:04 -0700

    RESET UNDONE

--- week07/ass2-photo-filter/js/main.js
+++ week07/ass2-photo-filter/js/main.js
@@ -1,92 +1,72 @@
 
+const SEARCHBAR = document.querySelector("#filter"); //filter functionality
+SEARCHBAR.addEventListener("input", onSearchItems); // Add an input event listener to the input
 
-//////PART A - user input search here
-const searchBar = document.querySelector("#filter"); //filter functionality
-//const tags = document.querySelectorAll(".tags");      //search all tags
-
-///// PART B - declare search and reset function here
-
-// Add an input event listener to the input
-searchBar.addEventListener("input", onSearchItems);
-//-----
 function onSearchItems(e){
-    const searchTerm = e.currentTarget.value.trim().toLowerCase();
-
-    // if(searchTerm === ""){  // get PART D - function for reset
-    //     resetTheDisplay();
-    // } else {    // get PART C - function for search
-    //     searchForMatchingItems(searchTerm);
-    // }
-
-    searchForMatchingItems(searchTerm);
+    const SEARCHTAG = e.currentTarget.value.trim().toLowerCase();
+    searchForMatchingItems(SEARCHTAG);
 }
-//-----
 
-
-//searchBar.addEventListener("input", searchForMatchingItems);
-
-///// PART C - function for search
-
-function searchForMatchingItems(searchTerm){
+function searchForMatchingItems(SEARCHTAG){
     // get all the thumb-display and the tags of it
     let thumbDisplay = document.querySelectorAll(".thumb-display");
-    //let tags = document.querySelectorAll(".tags");
-    let tags = document.querySelectorAll(".tags");      //search all tags
-    
+    //let tags = document.querySelectorAll(".tags");      //search all tags
     //// convert it into Array
     let thumbDisplayArray = Array.from(thumbDisplay);
-    let tagsArray = Array.from(tags);
-
-    //filter thumbDisplayArray using children
-    let thumbDisplayWithTags = thumbDisplayArray.filter((thumbdisplay) => { //thumbdisplay is a callback function
-    // console.log(thumb-display);
-    // console.log(thumb-display.children[1].innerText);
-    // console.log("------");
-    
-        if(!thumbdisplay.children[1].innerText.includes(searchTerm)){ // if NOT in the searchTerm (filter)
-            // image will not stay
-            //return true; 
-            //console.log("capt image");
-            //console.log(thumbdisplay);
-            thumbdisplay.style.display = "none"; // THIS IS THE ANSWER BABY!!!!!
-            //if there's a text in the filter
-            let resetShow = document.querySelector("a.reset");
-            //if (resetShow.classList.contains("hidden")){
-            if (searchTerm !== ""){
-                //resetShow.classList.add("hidden");
-                resetShow.style.visibility = "visible"; // THIS IS THE ANSWER BABY!!!!!
-                //resetShow.style.visibility = "hidden"; // THIS IS THE ANSWER BABY!!!!!
-            } else {
-                resetShow.style.visibility = "hidden"; // THIS IS THE ANSWER BABY!!!!!
+    //let tagsArray = Array.from(tags);
+    thumbDisplayArray.filter((thumbdisplay) => { //thumbdisplay is a callback function
+        let resetShow = document.querySelector("a.reset"); // variable for reset
+        if(thumbdisplay.children[1].innerText.includes(SEARCHTAG)){ // if in the SEARCHTAG (filter)
+            // if (SEARCHTAG !== ""){
+            //     //resetShow.classList.add("hidden");
+            //     resetShow.style.visibility = "visible"; 
+            //     //resetShow.style.visibility = "hidden"; 
+            // } else {
+            //     resetShow.style.visibility = "hidden"; 
+            //     //resetShow.style.visibility = "visible"; 
+            // }
+            //------
+            thumbdisplay.classList.add("hidden");
+            resetShow.style.visibility = "hidden"; 
+            //resetShow.classList.add("hidden");
+            //resetShow.style.display = "visible"; // THIS IS THE ANSWER BABY!!!!! 
+            if(thumbdisplay.classList.contains("hidden")){
+                thumbdisplay.classList.remove("hidden");
                 //resetShow.style.visibility = "visible"; // THIS IS THE ANSWER BABY!!!!!
-            }
-            
-        } 
-        //else {
-            // image will fadeout 
-            //return false;   
-            
-        //}
-        
+                //resetShow.classList.remove("hidden");
+                //resetShow.style.visibility = "visible"; // THIS IS THE ANSWER BABY!!!!!
+            } 
+        } else {
+            thumbdisplay.classList.add("hidden");
+            resetShow.style.visibility = "visible"; // THIS IS THE ANSWER BABY!!!!!
+        }     
     });
-
 }
 
-//// PART D - function for reset
-function resetTheDisplay(searchTerm) {
+function resetTheDisplay(SEARCHTAG) {
     let resetShow = document.querySelector("a.reset");
-    if(searchBar.contains(searchTerm)){
+    if(SEARCHBAR.contains(SEARCHTAG)){
         resetShow.classList.add("hidden");
     }
 }
 
-// function resetTheDisplay() {
-//     let resetShow = document.querySelector("a.reset");
-//     if(resetShow) {
-//         thumbdisplay.style.display = "visible"; 
-//     }
-// }
-//resetTheDisplay("click", );
+
+document.querySelector("a.reset").addEventListener("click", resetTheDisplay);
+function resetTheDisplay(){
+    // thumbDisplayArray.forEach(function(tag, index){
+    //     if( tag.classList.contains("hidden")){
+    //         tag.classList.remove("hidden")
+
+    //     }
+    // })
+    //SEARCHTAG= "";
+    console.log("click");
+    if (SEARCHBAR === ""){
+        thumbDisplayArray.classList.add("hidden");;
+    }
+
+}
+
 
 
 
