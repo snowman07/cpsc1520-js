caed0016b8a4057431d8fe6a38259ba5fdfe8095
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-12-11 14:54:08 -0700

    still in prog

--- week12/friend-finder/js/main.js
+++ week12/friend-finder/js/main.js
@@ -1,1 +1,21 @@
 // Your code here...
+
+let friends = [];
+let friendElements = [];
+
+fetch("./friend-finder/friends/friends.json")
+    .then((response) => response.json())
+    .then((data) => {
+        friends = [...data];
+        // function here
+
+    })
+    .catch((error) => console.warn(error));
+
+const createPostElements = () => {
+
+}
+
+const updatePostDisplay = () => {
+    
+}
