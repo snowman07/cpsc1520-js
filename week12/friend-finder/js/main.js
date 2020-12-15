848380c426d73f5ca6ec81554e78d3c7745da4be
snowman07 <59627816+snowman07@users.noreply.github.com>
2020-12-14 21:38:48 -0700

    add some codes

--- week12/friend-finder/js/main.js
+++ week12/friend-finder/js/main.js
@@ -1,21 +1,27 @@
 // Your code here...
 
+
+let friendsClick = document.querySelector(".friends");
+let homeClick = document.querySelector(".home");
 let friends = [];
 let friendElements = [];
 
-fetch("./friend-finder/friends/friends.json")
-    .then((response) => response.json())
-    .then((data) => {
-        friends = [...data];
-        // function here
 
-    })
-    .catch((error) => console.warn(error));
+friendsClick.addEventListener("click", function(e) {
+    fetch("./friend-finder/friends/friends.json")
+        .then((response) => response.json())
+        .then((data) => {
+            friends = [...data];
+            // function here
 
-const createPostElements = () => {
+        })
+        .catch((error) => console.warn(error));
 
-}
+    const createPostElements = () => {
 
-const updatePostDisplay = () => {
-    
-}
+    }
+
+    const updatePostDisplay = () => {
+        
+    }
+})
